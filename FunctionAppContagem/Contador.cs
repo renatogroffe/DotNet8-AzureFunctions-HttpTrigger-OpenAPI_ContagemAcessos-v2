a7544132cc032efadb2ab8341494d13391e11a42
Renato Groffe <renato.groffe@yahoo.com.br>
2024-05-05 16:55:46 -0300

    Commit inicial
    Commit inicial

---
+++ FunctionAppContagem/Contador.cs
@@ -0,0 +1,19 @@
+namespace FunctionAppContagem;
+
+public class Contador
+{
+    public const int INCREMENTO = 1;
+    private int _valorAtual = 0;
+
+    public int ValorAtual { get => _valorAtual; }
+
+    public void Incrementar()
+    {
+        _valorAtual += INCREMENTO;
+    }
+
+    public void Decrementar()
+    {
+        _valorAtual -= INCREMENTO;
+    }
+}
